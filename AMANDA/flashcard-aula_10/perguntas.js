261cee747c4cbb1b4b4bac4d67fa419a82adf3b3
Amandadossanto <161044216+Amandadossanto@users.noreply.github.com>
2024-11-01 10:49:32 -0300

    Add files via upload

---
+++ AMANDA/flashcard-aula_10/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação',
+    'O que é index.html ?',
+    'O arquivo index.html é o arquivo principal de um site web, sendo o primeiro a ser executado quando um usuário acessa a página inicial'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital dos Estados Unidos?',
+    'A capital do Estados Unidos Washington, D.C.'
+)
+
+criaCartao(
+    ' Arte',
+    'qual a obra mais famosa do Van Gogh?',
+    'Noite estrelada'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz como foi seu dia em inglês?',
+    'How was your day?'
+)
+
+criaCartao(
+    'Biologia',
+    'oq são briófitas?',
+    ' são plantas de pequeno porte encontradas principalmente em áreas úmidas'
+)
+
+criaCartao(
+    'história',
+    'que ano começou a primeira guerra ?',
+    ' 28 de Julho de 1914'
+)
